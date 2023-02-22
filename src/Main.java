b6e33010a1169a997181af871cab304eaa6283b8
YangYi <1134456853@qq.com>
2023-02-22 21:44:32 +0800

    用来测试在idea上的git的提交以及其他操作 已更改 第二次

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,10 @@
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!git3");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello world!333");
+        System.out.println("Hello world!333");
+        System.out.println("Hello world!333");
     }
 }
