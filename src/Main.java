cea55f34e05db9d0827a6ce4af3fe57f16ee7e68
YangYi <1134456853@qq.com>
2023-02-22 21:41:53 +0800

    用来测试在idea上的git的提交以及其他操作

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author yangyibufeng
+ * @Description
+ * @date ${DATE}-${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
