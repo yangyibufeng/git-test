47ed0a6d747570ac654912cb44bdd83804567837
yangyibufeng <98999768+yangyibufeng@users.noreply.github.com>
2023-02-22 22:13:41 +0800

    pull test
    测试在GitHub上修改完项目之后再pull到本地

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("Hello world! hot-fix 冲突测试");
         System.out.println("Hello world! master 冲突测试");
         System.out.println("Hello world! 测试在本地修改完项目之后再push到GitHub上");
+        System.out.println("Hello world! 测试在GitHub上修改完项目之后再pull到本地");
     }
 }
